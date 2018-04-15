65cd53d041d7494ccc3834d925f00f154871196b
DiegoEzequielWickert <dwdiegooo@gmail.com>
2018-04-15 17:26:07 -0300

    Configuração

--- site/index.php
+++ site/index.php
@@ -1,9 +1,14 @@
+<?php
+  session_start();
+  if (isset($_SESSION['usuario']));
+  //header('Location:/login/');
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1"/>
-  <title>Parallax Template - Materialize</title>
+  <title> Inicio | Crescer Contabilidade</title>
 
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -15,13 +20,20 @@
     <div class="nav-wrapper container">
       <a id="logo-container" href="#" class="brand-logo">Logo</a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="#">Navbar Link</a></li>
+        <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">home</i></a>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">A Crescer</a></li>
+        <li><a href="#">Serviços</a></li>
+        <li><a href="#">Clientes</a></li>
+        <li><a href="#">Representantes</a></li>
+        <li><a href="#contato">Contatos</a></li>
+        <li><a href="#">Intranet</a></li>
       </ul>
 
       <ul id="nav-mobile" class="sidenav">
-        <li><a href="#">Navbar Link</a></li>
+        <li><a href="#">A Crescer</a></li>
       </ul>
-      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">Menu</i></a>
     </div>
   </nav>
 
@@ -29,12 +41,12 @@
     <div class="section no-pad-bot">
       <div class="container">
         <br><br>
-        <h1 class="header center teal-text text-lighten-2">Parallax Template</h1>
+        <h1 class="header center teal-text text-lighten-2">Crescer Contabilidade</h1>
         <div class="row center">
-          <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+          <h5 class="header col s12 text-green text-darken-2"> Crescendo com  você!  </h5>
         </div>
         <div class="row center">
-          <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
+          <a href="#contato" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Quero entrar em contato!</a>
         </div>
         <br><br>
 
@@ -46,15 +58,15 @@
 
   <div class="container">
     <div class="section">
-
       <!--   Icon Section   -->
       <div class="row">
         <div class="col s12 m4">
           <div class="icon-block">
             <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
-            <h5 class="center">Speeds up development</h5>
+            <h5 class="center">Missão</h5>
 
-            <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+            <p class="light">
+              Missão  misão misão missao missao missao missao
           </div>
         </div>
 
@@ -79,6 +91,35 @@
 
     </div>
   </div>
+
+
+  <div class="row center">
+    <div class="col s12 m3">
+      <div class="card">
+        <div class="card-image">
+          <img class="responsive-img "src="img/controle-gerencial.jpg">
+          <span class="card-title">Controle Gerencial</span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red" href="/controle-gerencial"><i class="material-icons">add</i></a>
+        </div>
+        <div class="card-content ">
+          <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m3">
+      <div class="card">
+        <div class="card-image">
+          <img class="responsive-img "src="img/auditoria.png">
+          <span class="card-title">Auditoria</span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red" href="/controle-gerencial"><i class="material-icons">add</i></a>
+        </div>
+        <div class="card-content ">
+          <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+
+  </div> <!-- fim dos paineis-->
 
 
   <div class="parallax-container valign-wrapper">
@@ -118,11 +159,11 @@
     <div class="parallax"><img src="background3.jpg" alt="Unsplashed background img 3"></div>
   </div>
 
-  <footer class="page-footer teal">
+  <footer class="page-footer teal" id='contato'>
     <div class="container">
       <div class="row">
         <div class="col l6 s12">
-          <h5 class="white-text">Company Bio</h5>
+          <h5 class="white-text">Contatos</h5>
           <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
 
 
@@ -137,19 +178,19 @@
           </ul>
         </div>
         <div class="col l3 s12">
-          <h5 class="white-text">Connect</h5>
+          <h5 class="white-text">Link Úteis</h5>
           <ul>
             <li><a class="white-text" href="#!">Link 1</a></li>
             <li><a class="white-text" href="#!">Link 2</a></li>
-            <li><a class="white-text" href="#!">Link 3</a></li>
-            <li><a class="white-text" href="#!">Link 4</a></li>
+            <li><a class="white-text" href="#!"></a></li>
+            <li><a class="white-text" href="/login/">Login</a></li>
           </ul>
         </div>
       </div>
     </div>
     <div class="footer-copyright">
       <div class="container">
-      Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+        By <a class="brown-text text-lighten-3" href="#">WebFinatto.com</a>
       </div>
     </div>
   </footer>
