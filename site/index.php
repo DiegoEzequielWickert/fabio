c227b1e677fc9f8b318a30738423a31e22125e81
DiegoEzequielWickert <dwdiegooo@gmail.com>
2018-04-28 18:56:19 -0300

    Clientes

--- site/index.php
+++ site/index.php
@@ -149,16 +149,13 @@
       <div class="row">
         <div class="col s12 center">
           <h3><i class="mdi-content-send brown-text"></i></h3>
-          <h4>Contact Us</h4>
+          <h4>Contate Nós</h4>
           <p class="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
         </div>
       </div>
-
     </div>
   </div>
-
-
-  <div class="parallax-container valign-wrapper">
+    <div class="parallax-container valign-wrapper">
     <div class="section no-pad-bot">
       <div class="container">
         <div class="row center">
@@ -166,8 +163,33 @@
         </div>
       </div>
     </div>
-    <div class="parallax"><img src="background3.jpg" alt="Unsplashed background img 3"></div>
+    <div class="parallax"><img src="background2.jpg" alt="Unsplashed background img 3"></div>
   </div>
+  <!-- INICIO DOS CLIENTES-->
+  
+  <div class="container">
+    <div class="section">
+
+      <div class="row">
+        <div id="clientes" class="col s12 center">
+          <h3><i class="mdi-content-send brown-text"></i></h3>
+          <h4>Clientes</h4>
+          <p class="left-align light"> Junte-se a nós, seja um cliente Feliz </p>
+        </div>
+      </div>
+
+      <div class="carousel">
+      <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/250/250/nature/1"></a>
+      <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/250/250/nature/2"></a>
+      <a class="carousel-item" href="#three!"><img src="https://lorempixel.com/250/250/nature/3"></a>
+      <a class="carousel-item" href="#four!"><img src="https://lorempixel.com/250/250/nature/4"></a>
+      <a class="carousel-item" href="#five!"><img src="https://lorempixel.com/250/250/nature/5"></a>
+    </div>  
+  
+    </div>
+  </div>
+    
+  <!-- FIM DOS CLIENTES-->
 
   <footer class="page-footer teal" id='contato'>
     <div class="container ">
@@ -207,9 +229,21 @@
 
 
   <!--  Scripts-->
+  
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="js/materialize.js"></script>
   <script src="js/init.js"></script>
+  <script>
+      var elem = document.querySelector('.carousel');
+      var instance = M.Carousel.init(elem, options);
+
+      // Or with jQuery
+
+      $(document).ready(function(){
+        $('.carousel').carousel();
+      });
+      
+  </script>
 
   </body>
 </html>
